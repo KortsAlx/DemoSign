0475d96412915486e1bf84056639ec3f636b39ef
Brandon Alexis Martinez Cortes <brandon.martinez@docsolutions.com>
2022-09-28 16:29:42 -0500

    Initial Commit

---
+++ DemoSign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoSign
+//
+//  Created by Brandon Alexis Martinez Cortes on 28/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoSign/DemoSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSignApp.swift
+//  DemoSign
+//
+//  Created by Brandon Alexis Martinez Cortes on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
